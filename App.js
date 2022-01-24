cce23c1df0a0a8b3aaab2ba295578cbcf06aac14
Paulo Henrique <paulohenrique9400@gmail.com>
2022-01-24 13:53:40 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+export default App;
